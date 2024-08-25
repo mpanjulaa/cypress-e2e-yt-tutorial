172aa6db7f15871b5e2fdf555a08d54b58327e58
Anjula Malshani <mpanjulaa@gmail.com>
2024-08-25 19:53:08 +0530

    create test cases

--- cypress/e2e/Myfirsttest.cy.js
+++ cypress/e2e/Myfirsttest.cy.js
@@ -1,1 +1,17 @@
-npx
+
+
+describe('My First Test', () => {
+    it('Verify title-possitive', () => {
+
+     cy.visit("https://opensource-demo.orangehrmlive.com/")
+     cy.title().should('eq','OrangeHRM')
+
+    })
+
+    it('Verify title-negative test', () => {
+
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+        cy.title().should('eq','OrangeHRM123')
+   
+       })
+    })

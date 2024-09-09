eb0b65d533940b2c9362ad8ce19e74b361e3756c
Anjula Malshani <mpanjulaa@gmail.com>
2024-09-09 10:13:22 +0530

    admin login

--- cypress/e2e/Aplus_admin.cy.js
+++ cypress/e2e/Aplus_admin.cy.js
@@ -7,13 +7,18 @@ describe('admin login', () => {
      cy.visit("https://aplus-admin.web.app/auth/login")
      
      //fill in the form
-     cy.get ("#login-email",{timeout:5000}).type('testingone@apluseducation.lk')
-     cy.get ("#login-password",{timeout:5000}).type('5342118@axoten')
+     cy.get ("#login-email",{timeout:6000}).type('testingone@apluseducation.lk')
+     cy.get ("#login-password",{timeout:6000}).type('5342118@axoten')
      //sign in
-     cy.get ("#login-submit",{timeout:5000}).click()
-     cy.visit("https://aplus-admin.web.app/app/customers")
-     cy.get("#search-student",{timeout:5000}).type('762504597')
-     cy.get("#student-search-submit",{timeout:5000}).click()
+     cy.get ("#login-submit",{timeout:6000}).click()
+     cy.visit("https://aplus-admin.web.app/app/customers",{timeout:10000})
+     cy.get("#search-student",{timeout:9000}).type('762504597')
+     cy.get('[data-cy="student-search-submit"]',{timeout:7000}).click()
+     cy.get('[data-cy="view-student"]',{timeout:7000}).first().click()
+    
+     
+
+     
      
 })
 })

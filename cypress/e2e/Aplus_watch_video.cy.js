5b8dc40855df2bf328eca4b690f26fde8c3b4d74
mpanjulaa <mpanjulaa@gmail.com>
2024-11-18 20:35:14 +0530

    IMS

--- cypress/e2e/Aplus_watch_video.cy.js
+++ cypress/e2e/Aplus_watch_video.cy.js
@@ -2,7 +2,7 @@
 
 describe('Aplus Video', () => {
    
-    it('It user should be vissible video', () => {
+    it('It user should be vissible free video', () => {
         cy.visit("https://apluseducation.lk/auth/login")
         cy.get("#login-mobile",{timeout:6000}).type('0762504597')
         cy.get("#login-password",{timeout:6000}).type('2507ANJU@spw')
@@ -14,6 +14,6 @@ describe('Aplus Video', () => {
         cy.get('[data-cy="tutor-single"]',{timeout:8000}).click()
         cy.get('[data-cy="tutor-year-btn"]',{timeout:12000}).eq(3).click()
         cy.get('[data-cy="course-card"]',{timeout:12000}).first().click()
-        cy.get('[data-cy="lesson-month-btn"]',{timeout:12000}).eq(8).click()
-    })
+        cy.get('[data-cy="attend-free-btn"]',{timeout:12000}).first().click()
   })
+})
---
+++ cypress/e2e/IMS.cy.js
@@ -0,0 +1,24 @@
+//<reference types = 'cypress'/>
+describe('watch IMS Vedio', () => {
+   
+    it('It user should be vissible video', () => {
+ 
+        cy.visit("https://imsedu.lk/auth/login")
+        cy.get('[data-cy="login-mobile"]',{timeout:4000}).type('0760830538')
+        cy.get('[data-cy="login-password"]',{timeout:4000}).type('20011027R')
+        cy.get('[data-cy="login-submit"]',{timeout:4000}).click()
+        cy.wait(2000)
+
+        //visit courses
+        cy.visit("https://imsedu.lk/courses")
+        cy.wait(2000)
+        cy.get('[data-cy="subject-link"]',{timeout:4000}).first().click()
+        cy.wait(2000)
+        cy.get('[data-cy="tutor-single"]',{timeout:4000}).click()
+        cy.get('[data-cy="tutor-year-btn"]',{timeout:4000}).eq(2).click()
+        cy.get('[data-cy="course-card"]',{timeout:4000}).first().click()
+        cy.get('[data-cy="lesson-month-btn"]',{timeout:4000}).eq(6).click()
+        cy.get('[data-cy="attend-now"]',{timeout:4000}).first().click()
+        cy.get('[data-cy="model-attend-now"]',{timeout:4000}).click()
+    })
+})
